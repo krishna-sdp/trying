649978fe2a04671b8b2151da1bdd7c8c253824cc
Krishnadev Thakur <iamkrishnadev1999@gmail.com>
2022-01-05 14:41:17 +0530

    code upated

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,17 @@
 import React, { useEffect } from "react";
 // import { motion } from "framer-motion";
-import About from "../components/Home/About";
+// import About from "../components/Home/About";
 
 
-import WorkShocase from "../components/Home/WorkShocase";
-import Testimonials from "../components/Home/Testimonials";
-import ClientLogo from "../components/ClientLogo";
+// import WorkShocase from "../components/Home/WorkShocase";
+// import Testimonials from "../components/Home/Testimonials";
+// import ClientLogo from "../components/ClientLogo";
 // import Router from "next/router";
 // const Banners = dynamic(() => import("../components/Home/Banners"), {
 //   ssr: false,
 // });
 import Banners from "../components/Home/Banners";
-import Services1 from "../components/Home/Services1";
+// import Services1 from "../components/Home/Services1";
 // import { gsap } from "gsap";
 // import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 // import { CSSRulePlugin } from "gsap/dist/CSSRulePlugin";
