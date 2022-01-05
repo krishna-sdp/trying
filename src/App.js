c4b694fd496c4d4bcbd805db7de191bdee91a245
Krishnadev Thakur <iamkrishnadev1999@gmail.com>
2022-01-05 17:14:58 +0530

    yo-as

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/home';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { CSSPlugin } from "gsap/CSSPlugin";
+import gsap from "gsap";
+gsap.registerPlugin(CSSPlugin)
+const C = CSSPlugin;  // here is the gotcha....
+gsap.registerPlugin(ScrollTrigger);
+gsap.core.globals("ScrollTrigger", ScrollTrigger);
+gsap.core.globals("CSSPlugin", CSSPlugin);
 
 function App() {
   return (
